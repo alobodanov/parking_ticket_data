a1e1dd2fcab2e4ad7964e8a227c8160a1799c00a
Artem Lobodanov <artem.lobodanov@gmail.com>
2019-06-06 20:45:56 -0400

    connecting to db

--- app.py
+++ app.py
@@ -7,10 +7,16 @@ from flask import (
     request,
     redirect)
 
+import sqlalchemy as db
 
 app = Flask(__name__)
 
+engine = db.create_engine('dialect+driver://r92rmrsruojzupqe:v10ejjwsi5khwwdo@s3lkt7lynu0uthj8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/itp11fv3hiub8utw')
 
+connection = engine.connect()
+
+metadata = db.MetaData()
+census = db.Table('parking_ticket_data', metadata, autoload=True, autoload_with=engine)
 
 
 @app.route("/")
